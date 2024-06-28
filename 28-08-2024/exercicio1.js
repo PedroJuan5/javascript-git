775dae8400454e219d91e137b466d6f6505645af
PedroJuan5 <pd.juan05@gmail.com>
2024-06-28 11:45:31 -0300

    adicionando arquivos

---
+++ 28-08-2024/exercicio1.js
@@ -0,0 +1,9 @@
+/*
+-Escreva um loop (for)que exiba a frase abaixo no console
+-Subistitua X pela informação correta
+"X" deve começar em 1 na 1º exibição da frase e em 10 na ultima
+-Exiba a frase 10x no console
+*/
+for(let i = 1; i <= 10; i++){
+    console.log(`Esta é a ${i}º vez que esta frase é exibida no console `);
+}
---
+++ 28-08-2024/for2.js
@@ -0,0 +1,8 @@
+let frutas = ["maça", "banana","pera"];
+
+for(let i = 0; i < frutas.length; i++){
+console.log(`${frutas[i]}`)
+}
+
+
+
